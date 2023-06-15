c7237ac1d67ae654c3f1de49bc09bbc0c48e5854
Argeross <73711117+Argeross@users.noreply.github.com>
2023-06-16 00:27:38 +0200

    Added support for mDNS

--- cli.py
+++ cli.py
@@ -6,6 +6,8 @@ from functools import partial
 import argparse
 
 BUFF=1024
+MDNS_PORT=5454
+MDNS_ADDR='224.0.0.251'
 
 def signal_handler(signal, frame, client_socket):
     print("Closing client...")
@@ -20,6 +22,29 @@ def signal_handler(signal, frame, client_socket):
 
     client_socket.close()
     sys.exit(0)
+
+def get_server_ip_mdns(server_name="chat_server"):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    sock.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, 1)
+
+    sock.sendto(f"Hi, are you {server_name}?".encode('utf-8'), (MDNS_ADDR, MDNS_PORT))
+
+    sock.settimeout(10)
+    ip_address = None
+
+    while True:
+        try:
+            data, addr = sock.recvfrom(BUFF)
+        except socket.timeout:
+            print("Failed to find the server via mulitcast")
+            break
+        else:
+            if data.decode('utf-8') == 'Yup':
+                ip_address = addr[0]
+                break
+
+    sock.close()
+    return ip_address
 
 def get_ip_address(domain_name):
     try:
@@ -55,15 +80,25 @@ def start_cli(args):
     # Create a TCP socket
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    # Create a UDP socket
+    udp_client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     # Find server using DNS api
     if args.dns:
         ip_address = get_ip_address(str(args.dns))
         if ip_address == None:
             sys.exit(0)
         print("Got IP from getaddrinfo()", ip_address)
+    
+    #Find server using multicast (custom mDNS)
+    elif args.multicast:
+        ip_address = get_server_ip_mdns()
+        if ip_address == None:
+            sys.exit(0)
+        print("Got IP from mDNS", ip_address)
     else:
         ip_address = args.addr
-        
+    
     # Connect to the server 
     server_address = (ip_address, 4001)
     client_socket.connect(server_address)
@@ -97,10 +132,10 @@ def start_cli(args):
     sys.exit(0)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description="Choose method of connection and sending messages")
+    parser = argparse.ArgumentParser(description="Choose method of connection and sending messages.")
     parser.add_argument('-a','--addr', help="Server IP address.")
-    parser.add_argument('-d','--dns', help="Use DNS query to find IP address.")
-    parser.add_argument('-m', '--multicast', help="Create UDP client socket and connect to multicas group.")
+    parser.add_argument('-d','--dns', help="Use DNS query to find server's IP address.")
+    parser.add_argument('-m', '--multicast', help="Use custom mDNS to find server's IP address.")
     args = parser.parse_args()
     print(args)
     start_cli(args)
--- serv.py
+++ serv.py
@@ -2,15 +2,24 @@ import socket
 import threading
 import signal
 import sys
+import struct
 from functools import partial
 
 BUFF=1024
+MDSCV_PORT=5454
+MDSCV_ADDR='224.0.0.251'
+MDSCV_THREAD = True
 
 clients = list()
 
-def signal_handler(signal, frame, server_socket, clients):
+def signal_handler(signal, frame, server_socket, clients, mdns_socket):
     print("Closing server...")
 
+    try:
+        mdns_socket.shutdown(socket.SHUT_RD)
+    except OSError:
+        mdns_socket.close()
+    
     for client_socket in clients:
         client_socket.sendall("[Server shutdown]".encode('utf-8'))
         client_socket.close()
@@ -18,6 +27,24 @@ def signal_handler(signal, frame, server_socket, clients):
 
     server_socket.close()
     sys.exit(0)
+
+def send_ip_address_mdns(mdns_sock, server_name="chat_server"):
+    while True:
+        try:
+            data, address = mdns_sock.recvfrom(BUFF)
+
+            if address != None:
+                print(f"mDNS query from: {str(address)}")
+            
+            if not data:
+                break
+
+            if data.decode() == f"Hi, are you {server_name}?":               
+                mdns_sock.sendto("Yup".encode(), address)
+
+        except Exception as e:
+            print(f"Error: {e}, MDNS")
+            break
 
 def handle_client(client_socket):
     while True:
@@ -45,8 +72,12 @@ def start_serv():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    # Create a UDP socket
-    # PLACEHOLDER
+    # Create a UDP socket (for mDNS)
+    mdns_sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
+    mdns_sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    mdns_sock.bind(('', MDSCV_PORT))
+    mreq = struct.pack("4sl", socket.inet_aton(MDSCV_ADDR), socket.INADDR_ANY)
+    mdns_sock.setsockopt(socket.IPPROTO_IP, socket.IP_ADD_MEMBERSHIP, mreq)
 
     # Bind the socket to a specific address and port
     server_address = ('', 4001)
@@ -56,8 +87,12 @@ def start_serv():
     server_socket.listen(5)
     print("Server started. Waiting for connections...")
 
+    # mDNS thread
+    mdns_thread = threading.Thread(target=send_ip_address_mdns, args=(mdns_sock,))
+    mdns_thread.start()
+
     # Signal handler for termination
-    signal_handler_partial = partial(signal_handler, server_socket=server_socket, clients=clients)
+    signal_handler_partial = partial(signal_handler, server_socket=server_socket, clients=clients, mdns_socket=mdns_sock)
     signal.signal(signal.SIGINT, signal_handler_partial)
 
     while True:
